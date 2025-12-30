d21f95750e1818766b19c1f2cfb4e7951f6e501f
shambhuraj <shambhuraj960410054@gmail.com>
2025-12-30 10:12:58 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,28 @@ const nextConfig = {
   // Production optimizations
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.paypal.com https://sdk.cashfree.com",
+              "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.paypal.com https://sdk.cashfree.com",
+              "img-src 'self' blob: data: https: https://www.google-analytics.com https://shortlistai.xyz https://cdn.razorpay.com https://lh3.googleusercontent.com https://*.googleusercontent.com",
+              "connect-src 'self' https://www.google-analytics.com https://api.razorpay.com https://api.cashfree.com https://www.paypal.com https://*.paypal.com",
+              "frame-src 'self' https://checkout.razorpay.com https://www.paypal.com https://*.paypal.com https://sdk.cashfree.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
